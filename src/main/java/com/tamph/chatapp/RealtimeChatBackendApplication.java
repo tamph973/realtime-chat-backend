9c6a493570cf6b633cd2f3f523518ee777ab86a7
tamph973 <phtam0907@gmail.com>
2026-01-21 02:19:51 +0700

    chore: initialize Spring Boot project with Maven setup

---
+++ src/main/java/com/tamph/chatapp/RealtimeChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tamph.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeChatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeChatBackendApplication.class, args);
+    }
+
+}
